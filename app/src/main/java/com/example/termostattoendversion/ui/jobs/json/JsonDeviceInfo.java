19e72895c87f6de925af7576ab4c47ed2eb8f7a9
Timofey Konyshev <frastercom@gmail.com>
2022-04-23 15:50:24 +0300

    Первый коммит

---
+++ app/src/main/java/com/example/termostattoendversion/ui/jobs/json/JsonDeviceInfo.java
@@ -0,0 +1,59 @@
+package com.example.termostattoendversion.ui.jobs.json;
+
+public class JsonDeviceInfo {
+
+    private String id;
+    private String name;
+    private String login;
+    private String password;
+    private String type;
+    private String homeTopic;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getHomeTopic() {
+        return homeTopic;
+    }
+
+    public void setHomeTopic(String homeTopic) {
+        this.homeTopic = homeTopic;
+    }
+}
---
+++ app/src/main/java/com/example/termostattoendversion/ui/jobs/json/JsonStatusMessage.java
@@ -0,0 +1,14 @@
+package com.example.termostattoendversion.ui.jobs.json;
+
+public class JsonStatusMessage {
+
+    private Object status;
+
+    public Object getStatus() {
+        return status;
+    }
+
+    public void setStatus(Object status) {
+        this.status = status;
+    }
+}
